acaa07e5a7cac268c8617ee8923e7ba67ba02f3e
Lopan455 <tjohander@gmail.com>
2017-02-27 14:14:51 -0600

    clear button

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,29 +3,37 @@ console.log('client.js sourced properly');
 $(document).ready(function(){
   console.log('jq sourced properly');
 
+  $('#clearButton').on('click',function(){
+    //empty our Inputs
+    $('#num0').val('');
+    $('#num1').val('');
+    $('#outputP').text('');
+  })
+
   $('.operatorButton').on('click',function(){
     //what type of operator is this?
-  var myOperator = $(this).data('operator');
-  console.log('in operator button click',myOperator);
+    var myOperator = $(this).data('operator');
+    console.log('in operator button click',myOperator);
     //get user number inputs
-  var num0 =  $('#num0').val();
-  var num1 =  $('#num1').val();
+    var num0 =  $('#num0').val();
+    var num1 =  $('#num1').val();
     //assemble object to send to the server
-  var objectToSend = {
+    var objectToSend = {
       x: num0,
       y: num1,
       type: myOperator
     }
-  console.log('sending',objectToSend);
-  $.ajax({
+    console.log('sending',objectToSend);
+    $.ajax({
     type: "POST",
     url: "/math",
     data: objectToSend,
     success: function(response){
       console.log('back from ajax with: ',response);
       //receive and answer response.answer
+      $('#ouput').text(response.answer);
       //display the answer on the DOM
       }//end success function
-    });//end ajax
-  });//end click listener
-}); // end document.reay
+      });//end ajax
+    });//end click listener
+  }); // end document.reay
